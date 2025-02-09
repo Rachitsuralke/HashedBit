670b1e6bc2d64367a27491c87069712165d16356
Rachitsuralke <161618791+Rachitsuralke@users.noreply.github.com>
2025-02-09 21:53:02 +0530

    Add files via upload

---
+++ ass 3/question2/script.js
@@ -0,0 +1,12 @@
+let str = 'I love my India';
+
+// Split the string into words
+let words = str.split(' ');
+
+// Reverse the array of words
+let reversedWords = words.reverse();
+
+// Join the words back into a single string
+let reversedStr = reversedWords.join(' ');
+
+console.log(reversedStr);  // Output: 'India my love I
---
+++ ass 3/question9/index.js
@@ -0,0 +1,9 @@
+function countWords(paragraph) {
+    return paragraph.split(' ').length;
+  }
+  
+  // Example usage:
+  let paragraph = "Mr. Ram is a good person.";
+  console.log(countWords(paragraph));  
+  
+  
